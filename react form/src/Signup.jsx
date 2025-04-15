ee1040ba7af3840ba7366330c18c8c296a2899b7
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-04-15 23:15:24 +0530

    handled sign up form in both controlled and uncontrolled way

---
+++ react form/src/Signup.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './Signup.css';
+export const SignUp = ()=>{
+
+    const [data,setData]=useState({
+        firstName:"",
+        lastName:"",
+        password:"",
+        email:"",
+        contact:""
+    })
+
+    const handleSubmit=(e)=>{
+e.preventDefault();
+
+console.log(data);
+    }
+
+    const handleInput=(e)=>{
+        const {name,value}=e.target;
+
+        setData((prev)=>({...prev,[name]:value}));
+    }
+
+
+    // this is uncontrolled form where react does not know what kind of value input tag have 
+
+    // to make it controlled just passed value in the value attribute 
+    return <form onSubmit={handleSubmit} >
+
+    
+        <h1>SignUp</h1>
+    
+            <label htmlFor="firstName">FirstName</label>
+            <input type="text" name="firstName" id="firstName" onChange={handleInput} value={data.firstName}/>
+        
+    
+            <label htmlFor="lastName">lastName</label>
+            <input type="text" name="lastName" id="lastName" onChange={handleInput} />
+        
+    
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" onChange={handleInput} />
+        
+    
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" onChange={handleInput} />
+        
+    
+            <label htmlFor="Contact">Contact No.</label>
+            <input type="number" name="contact" id="Contact" onChange={handleInput}/>
+        
+        <button>Submit </button>
+
+    </form>
+}
