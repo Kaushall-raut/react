b20f52d15b18becaf3fb9668ffb5d2b6567cd089
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-04-28 09:51:05 +0530

    useRouteError hook

--- react-router/src/pages/ErrorPage.jsx
+++ react-router/src/pages/ErrorPage.jsx
@@ -1,6 +1,9 @@
-import { NavLink } from "react-router";
+import { NavLink, useRouteError } from "react-router";
 
 export const ErrorPage = () => {
+  const error = useRouteError(); //provide some data in the form of object through which you can return some jsx  it is an another way of showing error page
+  console.log(error);
+
   return (
     <>
       <h1>Page Not found go back to Home Page</h1>
