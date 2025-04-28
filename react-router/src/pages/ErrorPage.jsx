1e94bbe22bf02a43962ddb36c73a3c03f345fd46
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-04-28 11:54:03 +0530

    useNavigate hook

--- react-router/src/pages/ErrorPage.jsx
+++ react-router/src/pages/ErrorPage.jsx
@@ -1,13 +1,20 @@
-import { NavLink, useRouteError } from "react-router";
+import { NavLink, useNavigate, useRouteError } from "react-router";
 
 export const ErrorPage = () => {
   const error = useRouteError(); //provide some data in the form of object through which you can return some jsx  it is an another way of showing error page
   console.log(error);
 
+  const navigate = useNavigate(); ///returns a function
+  const handleNavigate = () => {
+    navigate(-1); //this will take the user to its previous visited page
+  };
+
   return (
     <>
       <h1>Page Not found go back to Home Page</h1>
       <NavLink to={"/"}>HomePage</NavLink>
+      <button onClick={handleNavigate}>GO back to previous page</button>{" "}
+      {/** useNavigate hook can also use to navigate the user from one page to another page */}
     </>
   );
 };
