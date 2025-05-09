40357eb68097dcffac7644c2cef10efa014038d9
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-05-10 00:14:21 +0530

    created accordion

--- accordion/src/components/Accordion.jsx
+++ accordion/src/components/Accordion.jsx
@@ -1,15 +1,36 @@
 import { Card } from "./Card";
 import data from "../api/data.json";
+import { useEffect, useState } from "react";
 
 export const Accordion = () => {
+  const [jsonData, setData] = useState([]);
+  useEffect(() => {
+    setData(data);
+  }, []);
+
+  const [active, setActive] = useState(false);
+
+  const handleButton = (id) => {
+    setActive((prev) => (prev === id ? false : id));
+  };
+
   return (
-    <div className="bg-[#08182b]  flex justify-center flex-col items-center ">
+    <div className="  flex justify-center flex-col items-center ">
       <h1 className="mt-4 text-4xl text-[#4C3BCF] mb-8  font-bold">
         Accordion in Rect
       </h1>
       <ul className="w-full">
-        {data.map((currentValue) => {
-          return <Card key={currentValue}  value={currentValue}/>;
+        {jsonData.map((currentValue) => {
+          return (
+            <Card
+              key={currentValue}
+              value={currentValue}
+              active={active === currentValue.id}
+              handleButton={() => {
+                handleButton(currentValue.id);
+              }}
+            />
+          );
         })}
       </ul>
     </div>
--- accordion/src/components/Card.jsx
+++ accordion/src/components/Card.jsx
@@ -1,19 +1,28 @@
+
 import "../App.css";
 // eslint-disable-next-line react/prop-types
-export const Card = ({ value }) => {
+export const Card = ({ value,handleButton,active }) => {
   // eslint-disable-next-line react/prop-types
   const { question, answer } = value;
+
+
+
+
+
   return (
-    <section className="w-full flex justify-center mb-4 ">
-      <div className="border box-shadow  border-l-teal-500 grid grid-cols-2     w-[50%] p-4  ">
-        <div className="col-span-2 flex  justify-between">
+    <section className="w-full  flex justify-center mb-4 ">
+      <div className="border box-shadow  border-l-teal-500     w-[50%] p-4  ">
+        <div className="col-span-2 flex  justify-between ">
           <p className=" text-white text-[1.5rem] items-baseline">{question}</p>
-          <button className="bg-blue-800 rounded-md h-8 w-14  cursor-pointer hover:text-white ">
-            Show
+          <button
+            className={!active? "bg-blue-800 rounded-md h-8 w-14  cursor-pointer hover:text-white":"bg-red-800 rounded-md h-8 w-14  cursor-pointer hover:text-white"   }
+            onClick={handleButton}
+          >
+            {active ? "close" : "Show"}
           </button>
         </div>
         <div className="mt-4">
-          <p className="text-white">{answer}</p>
+          {active && <p className="text-white">{answer}</p>}
         </div>
       </div>
     </section>
